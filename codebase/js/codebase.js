98627d5b30ed40f403f97c857dee83fa621b167a
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-10-22 08:01:37 +0100

    Update codebase.js

--- codebase/js/codebase.js
+++ codebase/js/codebase.js
@@ -6,8 +6,8 @@
   $(document).ready(function() {
 
     // Menubar
-		$('.menubar').prepend('<span class="menubar-toggle"><i class="mega-octicon octicon-three-bars"></i></span>');
-		$('.menubar-toggle').click(function() {
+    $('.menubar').prepend('<span class="menubar-toggle"><i class="mega-octicon octicon-three-bars"></i></span>');
+    $('.menubar-toggle').click(function() {
       $(this).closest('.menubar').toggleClass('menubar-js-open');
     });
 
