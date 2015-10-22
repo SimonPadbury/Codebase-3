13da2c8f1476a0e94bf24d81c49e9ce61edc05d5
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-10-22 08:00:14 +0100

    Update codebase.js

--- codebase/js/codebase.js
+++ codebase/js/codebase.js
@@ -1,2 +1,15 @@
 // CODEBASE
 // ========
+
+(function($) {
+  "use strict";
+  $(document).ready(function() {
+
+    // Menubar
+		$('.menubar').prepend('<span class="menubar-toggle"><i class="mega-octicon octicon-three-bars"></i></span>');
+		$('.menubar-toggle').click(function() {
+      $(this).closest('.menubar').toggleClass('menubar-js-open');
+    });
+
+  });
+}(jQuery));
