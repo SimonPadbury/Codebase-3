9f3029caf6b21223acb74226fea7f1a66b98fbfd
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-10-24 09:03:53 +0100

    Update codebase.js

--- codebase/js/codebase.js
+++ codebase/js/codebase.js
@@ -5,7 +5,26 @@
   "use strict";
   $(document).ready(function() {
 
+    // Reset overlay
+
+    $('body').prepend('<span class="reset-overlay"></span>');
+
+    function reset_overlay() {
+      // Put everything that needs .reset-overlay activity in here
+      $('.reset-overlay').removeClass('reset-overlay-is-active');
+      $('.button-dropdown').removeClass('button-dropdown-is-revealed');
+    }
+
+    $('.reset-overlay').click(function() {
+      reset_overlay();
+    });
+
+    $(window).resize(function() {
+      reset_overlay();
+    });
+
     // Menubar
+
     $('.menubar').prepend('<span class="menubar-toggle"><i class="mega-octicon octicon-three-bars"></i></span>');
     $('.menubar-toggle').click(function() {
       $(this).closest('.menubar').toggleClass('menubar-js-open');
