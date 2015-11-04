b51c2e8e97da1bb604a2b7b2cceee4883d1b196f
Simon Padbury <simon_padbury@yahoo.co.uk>
2015-11-04 08:09:08 +0000

    update

--- codebase/js/codebase.js
+++ codebase/js/codebase.js
@@ -30,5 +30,35 @@
       $(this).closest('.menubar').toggleClass('menubar-js-open');
     });
 
+    // Button dropdowns
+
+    $('.button-dropdown').append('&nbsp;<i class="octicon octicon-triangle-down"></i>');
+    $('.button-dropdown').click(function(e) {
+      e.preventDefault();
+      if (!$(this).hasClass('button-dropdown-is-revealed')) {
+        $(this).addClass('button-dropdown-is-revealed');
+        $('.reset-overlay').addClass('reset-overlay-is-active');
+      } else {
+        reset_overlay();
+      }
+    });
+    $('.button-dropdown span').click(function(e) {
+      e.stopPropagation();
+    });
+
+    // Tabs
+
+    $('.tab-label:first-of-type').addClass('is-front');
+    $('.tab-label:first-of-type + .tab-card').addClass('is-front');
+
+    $('.tab-label').click(function() {
+      var tab_id = $(this).attr('data-tab');
+
+      $(this).siblings().removeClass('is-front');
+
+      $(this).addClass('is-front');
+      $("#" + tab_id).addClass('is-front');
+    });
+
   });
 }(jQuery));
